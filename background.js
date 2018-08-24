5ddb2332d27c1528219b384c26c19472f8bd594d
SyedHTanveer <Syedt21@gmail.com>
2018-08-24 12:21:56 -0400

    updated content.js so that when proper legislation is highlighted, it would be turned into a link that would go to the summary of the legistlation.

--- background.js
+++ background.js
@@ -1,14 +1,14 @@
-
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
-  $.ajax({
-    url: request.text,
-    cache: false,
-    success: function(response) {
-        result = $(response).find("#result");
-        // alert(response); // works as expected (returns all html)
-        // alert(result); // returns [object Object]
-    }
-  });
-});
+// 
+//
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//
+//   $.ajax({
+//     url: request.text,
+//     cache: false,
+//     success: function(response) {
+//         result = $(response).find("#result");
+//         // alert(response); // works as expected (returns all html)
+//         // alert(result); // returns [object Object]
+//     }
+//   });
+// });
--- content.js
+++ content.js
@@ -1,45 +1,33 @@
 
-$(document).ready(function() {
-            $('.tooltip').tooltipster();
-        });
-
-
-$('.tooltip').tooltipster({
-            theme: 'tooltipster-shadow'
-        });
-
-
 window.addEventListener('mouseup', function () {
   let selectedText = window.getSelection().toString();
   console.log(selectedText);
 
   let mainText = selectedText;
 
-  if (selectedText.slice(0,2) == "HR" || selectedText.slice(0,4) == "H.R." || selectedText.slice(0,2) == "S.") {
+  if (selectedText.slice(0,2) == "HR" || selectedText.slice(0,4) == "H.R." || selectedText.slice(0,2) == "S." || selectedText.slice(0,2) == "S ") {
 
-    if (selectedText.slice(0,2) == "HR") selectedText = selectedText.slice(2, selectedText.length);
-    if (selectedText.slice(0,4) == "H.R.") selectedText = selectedText.slice(4, selectedText.length);
-    if (selectedText.slice(0,2) == "S.") selectedText = selectedText.slice(2, selectedText.length);
-    selectedText = selectedText.replace(/\s+/g, '');
-    selectedText = 'https://www.congress.gov/bill/115th-congress/house-bill/' + selectedText;
-    console.log(selectedText);
-    let message = {
-      text: selectedText
-    };
+    if (selectedText.slice(0,2) == "HR" || selectedText.slice(0,2) == "hr" ) {
+      selectedText = selectedText.slice(2, selectedText.length);
+      selectedText = selectedText.replace(/\s+/g, '');
+      selectedText = 'https://www.congress.gov/bill/115th-congress/house-bill/' + selectedText;
+    }
+
+    if (selectedText.slice(0,4) == "H.R." || selectedText.slice(0,4) == "h.r.") {
+      selectedText = selectedText.slice(4, selectedText.length);
+      selectedText = selectedText.replace(/\s+/g, '');
+      selectedText = 'https://www.congress.gov/bill/115th-congress/house-bill/' + selectedText;
+
+    }
+
+    if (selectedText.slice(0,2) == "S." || selectedText.slice(0,2) == "S ") {
+      selectedText = selectedText.slice(2, selectedText.length);
+      selectedText = selectedText.replace(/\s+/g, '');
+      selectedText = 'https://www.congress.gov/bill/115th-congress/senate-bill/' + selectedText;
+    }
 
     $("body:contains(" + mainText + ")").html(function(_, html) {
-       return html.split(mainText).join("<span class='tooltip' title='This is my span\'s tooltip message!'>" + mainText + "</span>");
+       return html.split(mainText).join("<a href=" + selectedText + ">" + mainText + "</a>");
     });
-
-    chrome.runtime.sendMessage(message);
-  }
-
-  // chrome.runtime.onMessage.addListener(
-  //   function(request, sender, sendResponse) {
-  //
-  //
-  //
-  //   }
-  // });
-
+  };
 });

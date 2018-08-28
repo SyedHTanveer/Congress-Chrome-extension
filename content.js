03668d0222ab0c74335e9ae23d7af704e68036bd
SyedHTanveer <Syedt21@gmail.com>
2018-08-28 19:02:15 -0400

    deleted background.js and made the AJAX request happen in content.js instead.

--- content.js
+++ content.js
@@ -10,24 +10,24 @@ window.addEventListener('mouseup', function () {
     if (selectedText.slice(0,2) == "HR" || selectedText.slice(0,2) == "hr" ) {
       selectedText = selectedText.slice(2, selectedText.length);
       selectedText = selectedText.replace(/\s+/g, '');
-      selectedText = 'https://www.congress.gov/bill/115th-congress/house-bill/' + selectedText;
+      selectedText = 'https://www.congress.gov/bill/115th-congress/house-bill/' + selectedText + '/titles';
     }
 
     if (selectedText.slice(0,4) == "H.R." || selectedText.slice(0,4) == "h.r.") {
       selectedText = selectedText.slice(4, selectedText.length);
       selectedText = selectedText.replace(/\s+/g, '');
-      selectedText = 'https://www.congress.gov/bill/115th-congress/house-bill/' + selectedText;
-
+      selectedText = 'https://www.congress.gov/bill/115th-congress/house-bill/' + selectedText + '/titles';
     }
 
     if (selectedText.slice(0,2) == "S." || selectedText.slice(0,2) == "S ") {
       selectedText = selectedText.slice(2, selectedText.length);
       selectedText = selectedText.replace(/\s+/g, '');
-      selectedText = 'https://www.congress.gov/bill/115th-congress/senate-bill/' + selectedText;
+      selectedText = 'https://www.congress.gov/bill/115th-congress/senate-bill/' + selectedText + '/titles';
     }
 
     $("body:contains(" + mainText + ")").html(function(_, html) {
        return html.split(mainText).join("<a href=" + selectedText + ">" + mainText + "</a>");
-    });
+     }
+   );
   };
 });

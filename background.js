67a4be6ae82708f0d7089e048ec01d70783ea7b7
SyedHTanveer <Syedt21@gmail.com>
2018-08-31 12:11:01 -0400

    added style fixes as well as included title in the tooltip that displays on the page. Also added parsers that will cleanup the text for display in background.js.

--- background.js
+++ background.js
@@ -1,16 +1,60 @@
+// Author: Syed Tanveer
+// Last modified: August 31st, 2018
+// OCIO: Congress.gov chrome extension
+//
+// this function will send an AJAX request to the constructed link in content.js
+// and return the HTML of it. Then, the function will parse the HTML to gather
+// the title of the legislation and the number of summaries of the legislation.
+// Once that is complete and the HTML has been parsed properly, it will construct
+// a message object and send it back to conent.js to be rendered onto the page.
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     $.ajax({
     url: request.text,
         type:'GET',
         success: function(data) {
-           let message = $(data).find('#summarySelector').html();
-           
-           console.log(message);
+
+          let message = {
+            summary: '',
+            title: ''
+          };
+
+           let leg_Summary = $(data).find('#summarySelector').html(); // assigns the HTML for summary to a variable
+           let leg_Title = $(data).filter('title').text(); // assigns the Title of the legislation to a variable
+
+           console.log(leg_Title);
+           console.log(leg_Summary);
+
+           let endpoint = 0; // this variable will be used to hold the index of where the string will be parsed to.
+
+           // iterates through the summary string to delete the CRS link and cleans the text up for proper display
+           for (i=0; i < leg_Summary.length; i++) {
+             if(leg_Summary[i] == '.' && leg_Summary[i-1] == request.leg[request.leg.length-1]) {
+               endpoint = i;
+               break;
+             }
+           }
+
+           leg_Summary = leg_Summary.slice(4, endpoint); // creates the new string that will be displayed
+
+           // iterates through the title string to delete the tags and uneeded text for proper display
+           for (i=0; i < leg_Title.length; i++) {
+             if(leg_Title[i] == '|') {
+               endpoint = i-1;
+               break;
+             }
+           }
+
+           leg_Title = leg_Title.slice(7, endpoint); // creates the new string that will be displayed
+
+           // adding the newely parsed strings to the message we will send to content.js
+           message.summary = leg_Summary;
+           message.title = leg_Title;
 
            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
              console.log(message);
-             chrome.tabs.sendMessage(tabs[0].id, message);
+             chrome.tabs.sendMessage(tabs[0].id, message); // sends the message to content.js
          });
         }
     });
--- content.js
+++ content.js
@@ -1,16 +1,24 @@
+// Author: Syed Tanveer
+// Last modified: August 31st, 2018
+// OCIO: Congress.gov chrome extension
+
+// This function will take the highlighted text, parse it to make sure it is in
+// the proper format of the legislation, creates the weblink for that legislation
+//  and sends all of that information to the background.js file. Once the information
+//  has been sent to background.js, it will lookup the HTML of that page and send
+//  a message to content.js that contains the summary information.
 
 window.addEventListener('mouseup', function () {
-  let selectedText = window.getSelection().toString();
-  console.log(selectedText);
+  let selectedText = window.getSelection().toString(); //get the highlighted text
 
-  let mainText = selectedText;
+  let mainText = selectedText; // mainText will be the actual highlighted text, selectedText will be minipulated
 
-  if (selectedText.slice(0,2) == "HR" || selectedText.slice(0,4) == "H.R." || selectedText.slice(0,2) == "S." || selectedText.slice(0,2) == "S ") {
+  if (selectedText.slice(0,2) == "HR" || selectedText.slice(0,4) == "H.R." || selectedText.slice(0,2) == "S." || selectedText.slice(0,2) == "S ") { // checks to see proper format for the highlighted text
 
     if (selectedText.slice(0,2) == "HR" || selectedText.slice(0,2) == "hr" ) {
-      selectedText = selectedText.slice(2, selectedText.length);
-      selectedText = selectedText.replace(/\s+/g, '');
-      selectedText = 'https://www.congress.gov/bill/115th-congress/house-bill/' + selectedText;
+      selectedText = selectedText.slice(2, selectedText.length); // takes only the number portion of the highlighted text
+      selectedText = selectedText.replace(/\s+/g, ''); // removes white spaces
+      selectedText = 'https://www.congress.gov/bill/115th-congress/house-bill/' + selectedText; // creates the weblink
     }
 
     if (selectedText.slice(0,4) == "H.R." || selectedText.slice(0,4) == "h.r.") {
@@ -25,21 +33,23 @@ window.addEventListener('mouseup', function () {
       selectedText = 'https://www.congress.gov/bill/115th-congress/senate-bill/' + selectedText;
     }
 
-    console.log(selectedText);
+    // in order for the tooltip to be injected, we need to make the highlighted text a span element
     $("body:contains(" + mainText + ")").html(function(_, html) {
        return html.split(mainText).join("<a class='tooltip' href='" + selectedText +"'>" + mainText + "<span class='tooltiptext'>" + "loading..." + "</span></a>");
      });
 
     let message = {
-      text: selectedText
+      text: selectedText,
+      leg: mainText
      };
 
-    chrome.runtime.sendMessage(message);
+    chrome.runtime.sendMessage(message); // sends message to backgruond.js
   };
 });
 
+// This function is for receiving the constructed message from background.js
+//
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(request);
-     $( ".tooltiptext" ).html(request);
+    $( ".tooltiptext" ).html("<strong>" + request.title + "</strong>" + "<br />" + request.summary); // takes the summary HTML and displays it onto the page
 });
